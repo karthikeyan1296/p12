6131a44d256d09e487f81fa320192efdefb8244b
unknown <karthikeyan1296@yahoo.com>
2020-08-07 19:55:47 +0530

    specific static file

--- p12/settings.py
+++ p12/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR=os.path.join(BASE_DIR,"templates")
+STATIC_DIR_RSK=os.path.join(os.path.join(BASE_DIR,"rsk"),"static")
 STATIC_DIR=os.path.join(BASE_DIR,"static")
 
 # Quick-start development settings - unsuitable for production
@@ -119,7 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
+STATIC_ROOT=STATIC_DIR
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
-    STATIC_DIR,
+    STATIC_DIR_RSK,
 ] 
